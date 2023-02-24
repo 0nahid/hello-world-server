a4c90cbf3d56d825dbd0282bee673bd78c1d36d0
0nahid <nahidhassanbulbul@gmail.com>
2023-02-24 17:59:48 +0600

    aws

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World from aws!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
